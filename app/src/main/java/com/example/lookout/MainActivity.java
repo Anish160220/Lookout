8f6fa3b0252fcd2b0ff0d4b651de9fc28a855dcc
Anish <anishkalpakunj@gmail.com>
2019-04-21 16:46:29 +0545

    Google map Api was loaded

--- app/src/main/java/com/example/lookout/MainActivity.java
+++ app/src/main/java/com/example/lookout/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, MapsActivity.class);
-
+                startActivity(intent);
             }
         });
     }
